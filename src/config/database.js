3acc38a8c7f5297466c3f5f68c60c3e8e66f18df
Kehinde Yusuf <95845686+barakkenny@users.noreply.github.com>
2025-08-08 23:23:07 +0100

    Added the .env file

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,12 @@
 const mongoose = require("mongoose");
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const MONGO_PASS = process.env.MONGO_URI
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://barakkenny500:FIKoL5tStZfsZR8A@de.uyjuway.mongodb.net/")
+    await mongoose.connect(`mongodb+srv://barakkenny500:${MONGO_PASS}.uyjuway.mongodb.net/`)
 };
 
 module.exports = connectDB;
